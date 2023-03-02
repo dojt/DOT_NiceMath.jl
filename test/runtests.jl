95e33de267a7a09247ce7a2aab83a0ef6bd08d08
dojt <dotheis.work@gmail.com>
2023-03-02 17:47:39 +0200

    add (empty) tests

---
+++ test/runtests.jl
@@ -0,0 +1,18 @@
+########################################################################
+#                                                                      #
+# DOT_NiceMath/test/runtests.jl                                        #
+#                                                                      #
+# (c) Dirk Oliver Theis 2022                                           #
+#                                                                      #
+#                                                                      #
+# License:                                                             #
+#                                                                      #
+#             CC-by                                                    #
+#                                                                      #
+#             Creative Commons, share with attribution                 #
+#                                                                      #
+########################################################################
+
+
+using DOT_NiceMath
+
